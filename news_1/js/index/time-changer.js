e2e889c5c523e4193b402263f70508f65736b4bb
InancsiKrisztian1 <inancsikrisztian@outlook.com>
2025-01-28 13:18:46 +0100

    changes - 20250128 - Delete

--- news_1/js/index/time-changer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const yearSpan = document.getElementById('yearSpan');
-
-    function yearChange(){
-        const date = new Date();
-        yearSpan.textContent = date.getFullYear();
-    }
-    yearChange();
-});
