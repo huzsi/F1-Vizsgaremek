3a5f589969600fcf51195ffd7e4e83da8819f70e
InancsiKrisztian1 <inancsikrisztian@outlook.com>
2025-03-07 09:54:13 +0100

    changes - 20250307
    Server.js frissítése

--- news-vegleges/application/server/server.js
+++ news-vegleges/application/server/server.js
@@ -1,20 +1,3 @@
-/**Teendők:
- *      scrollmenü elemek méretezése reszponzivitásnál.
- *      profile.html - last interaction rész megcsinálása
- *      adminpanel - profilok törlése
- *      creator script átnézése.
- *      frontedjét is.
- *      Standlist gomb megcsinálása az indexen.
- *      Profilkép lehetőség.
- *      driver fotók kigyűjtése
- *      forum, topicok törlése.
- *      admin profil esetén oda is egy adminpanel.
- * 
- * Megoldani, hogy egy adatsor esetén is belegyenek töltve adatok, ne csak 2 vagy annál több. (Race result, Fórum - commentek, result.html).
- *      adatbázis bővítés - utolsó 5 futamgyőztes.
- *      Where can I watch - gomb megcsinálása.
- *      animációk.
- */
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
@@ -22,38 +5,17 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const NodeCache = require('node-cache');
-const cache = new NodeCache({ stdTTL: 3600 });
-const apiKey = 'bd49adc7fe7341ddb75478209aa97049';
+const apiKey = 'dbe6c673af274063be29c9aa0d09e5ed';
+(async () => {
+    const fetch = await import('node-fetch');
+    const response = await fetch.default(`https://newsapi.org/v2/everything?q=Formula%201&apiKey=${apiKey}`);
+    const data = await response.json();
+})();
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
+
 const newsUrl = `https://newsapi.org/v2/everything?q=Formula%201&apiKey=${apiKey}`;
-const techUrl = `https://newsapi.org/v2/everything?q=F1 AND Formula 1 AND technical&qInTitle=Analysis&apiKey=${apiKey}`;
-
-const updateCache = async () => {
-    const fetch = (await import('node-fetch')).default;
-    try {
-        const [newsResponse, techResponse] = await Promise.all([
-            fetch(newsUrl),
-            fetch(techUrl),
-        ]);
-
-        const newsData = await newsResponse.json();
-        const techData = await techResponse.json();
-       
-
-        cache.set('news', newsData);
-        cache.set('techNews', techData);
-      
-
-        console.log('Cache frissítve');
-    } catch (error) {
-        console.error('Cache frissítési hiba:', error);
-    }
-};
-
-updateCache();
-setInterval(updateCache, 3600*1000); //Időzítő, hogy mindig frissüljön.
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -102,30 +64,29 @@ app.get('/news/auth.html', servePage('auth'));
 app.get('/news/tracks.html', servePage('tracks'));
 app.get('/news/start.html', servePage('start'));
 app.get('/news/profile.html', servePage('profile'));
-app.get('/news/raceresult.html', servePage('raceresult'));
-app.get('/news/test.html', servePage('test'));
-app.get('/news/news', (req, res) => {
-    const newsData = cache.get('news');
-    if (newsData) {
-        res.json(newsData.articles);
-    } else {
-        res.status(500).json({ error: 'Cache üres' });
-    }
-});
 
-app.get('/news/tech-news', (req, res) => {
-    const techData = cache.get('techNews');
-    if (techData) {
-        res.json(techData.articles);
-    } else {
-        res.status(500).json({ error: 'Cache üres' });
+app.get('/news/news', async (req, res) => {
+    try {
+        const response = await fetch(newsUrl);
+        if (!response.ok) {
+            throw new Error(`API kérés hiba: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        if (data.status === 'ok') {
+            res.json(data.articles);
+        } else {
+            res.status(500).json({ error: 'Hiba történt a hírek lekérése során' });
+        }
+    } catch (error) {
+        console.error('Hálózati hiba:', error);
+        res.status(500).json({ error: 'Hálózati hiba történt' });
     }
 });
 app.get('/news/news-layout.html/:page', (req, res) => {
     const { page } = req.params;
     let filePath = path.join(__dirname, 'public', 'html', 'news-layout.html');
 
-    if (page === 'tech-news' || page === 'regular-news' || page === 'news') {
+    if (page === 'article' || page === 'tech-news' || page === 'regular-news') {
         res.sendFile(filePath);
     } else {
         res.status(404).send('Page not found');
@@ -141,17 +102,7 @@ app.get('/news/script-layout.html/:page', (req, res) => {
         res.status(404).send('Page not found');
     }
 });
-app.get('/news/forum-layout.html/:page', (req, res) => {
-    const { page } = req.params;
-    let filePath = path.join(__dirname, 'public', 'html', 'forum-layout.html');
 
-    if(page === 'index' || page === 'topic'){
-        res.sendFile(filePath);
-    }
-    else{
-        res.status(404).send('Page not found');
-    }
-})
 app.get('/news/race-schedule', (req, res) => {
     queryDB(res, `
         SELECT rd.id, rd.type, rd.event1, rd.event2, rd.event3, rd.event4, rd.event5, 
@@ -161,9 +112,13 @@ app.get('/news/race-schedule', (req, res) => {
         ORDER BY rn.raceNumber ASC
     `);
 });
+
 app.get('/news/trackinfo', (req, res) => {
     if (!req.query.id) return res.status(400).json({ error: "Missing required parameter: id" });
     queryDB(res, 'SELECT trackName, fullName FROM racenames WHERE id = ?', [req.query.id]);
+});
+app.get('/news/racenames', (req, res) =>{
+    queryDB(res,'SELECT raceNumber, id, name, fullName, trackName FROM racenames ORDER BY raceNumber ASC',[req.query.id]);
 });
 app.get('/news/circuitdatas', (req, res) => {
     if (!req.query.id) return res.status(400).json({ error: "Missing required parameter: id" });
@@ -171,48 +126,17 @@ app.get('/news/circuitdatas', (req, res) => {
 });
 
 app.get('/news/driverStandlist', (req, res) => {
-    queryDB(res, `SELECT driverId,driverName,constructor FROM driverNames`);
+    queryDB(res, `SELECT driverId,driverName,constructor FROM standlist`);
 });
 
 app.get('/news/constructorStandlist', (req, res) => {
-    queryDB(res, `SELECT DISTINCT constructorName, driverNames.constructor FROM constructornames INNER JOIN driverNames ON constructornames.constructorId = driverNames.constructorId`);
+    queryDB(res, `SELECT DISTINCT constructorName, standlist.constructor FROM constructornames INNER JOIN standlist ON constructornames.constructorId = standlist.constructorId`);
 });
 
 app.get('/news/seasonRaceResults', (req, res) => {
     queryDB(res, `SELECT * FROM seasonRaceResult`);
 });
-app.get('/news/raceResults', (req, res) => {
-    queryDB(res, `  SELECT 
-                    srr.raceId,
-                    rn.name AS raceName,
-                    d1.driverName AS P1, d2.driverName AS P2, d3.driverName AS P3, d4.driverName AS P4, d5.driverName AS P5,
-                    d6.driverName AS P6, d7.driverName AS P7, d8.driverName AS P8, d9.driverName AS P9, d10.driverName AS P10,
-                    d11.driverName AS P11, d12.driverName AS P12, d13.driverName AS P13, d14.driverName AS P14, d15.driverName AS P15,
-                    d16.driverName AS P16, d17.driverName AS P17, d18.driverName AS P18, d19.driverName AS P19,d20.driverName AS P20
-                    FROM seasonRaceResult srr
-                    JOIN raceNames rn ON srr.raceId = rn.id
-                    LEFT JOIN driverNames d1 ON srr.P1 = d1.driverId
-                    LEFT JOIN driverNames d2 ON srr.P2 = d2.driverId
-                    LEFT JOIN driverNames d3 ON srr.P3 = d3.driverId
-                    LEFT JOIN driverNames d4 ON srr.P4 = d4.driverId
-                    LEFT JOIN driverNames d5 ON srr.P5 = d5.driverId
-                    LEFT JOIN driverNames d6 ON srr.P6 = d6.driverId
-                    LEFT JOIN driverNames d7 ON srr.P7 = d7.driverId
-                    LEFT JOIN driverNames d8 ON srr.P8 = d8.driverId
-                    LEFT JOIN driverNames d9 ON srr.P9 = d9.driverId
-                    LEFT JOIN driverNames d10 ON srr.P10 = d10.driverId
-                    LEFT JOIN driverNames d11 ON srr.P11 = d11.driverId
-                    LEFT JOIN driverNames d12 ON srr.P12 = d12.driverId
-                    LEFT JOIN driverNames d13 ON srr.P13 = d13.driverId
-                    LEFT JOIN driverNames d14 ON srr.P14 = d14.driverId
-                    LEFT JOIN driverNames d15 ON srr.P15 = d15.driverId
-                    LEFT JOIN driverNames d16 ON srr.P16 = d16.driverId
-                    LEFT JOIN driverNames d17 ON srr.P17 = d17.driverId
-                    LEFT JOIN driverNames d18 ON srr.P18 = d18.driverId
-                    LEFT JOIN driverNames d19 ON srr.P19 = d19.driverId
-                    LEFT JOIN driverNames d20 ON srr.P20 = d20.driverId;
-                `);
-});
+
 app.post('/news/saveRaceResults', (req, res) => {
     const { raceId, results } = req.body;
     const query = `
@@ -228,88 +152,7 @@ app.post('/news/saveRaceResults', (req, res) => {
     ];
     queryDB(res, query, values);
 });
-///'SELECT * FROM forumTopics'
-app.get('/news/forumTopics', (req, res) => {
-    queryDB(res, `SELECT tc.topicId, u.usernames, tc.userId, tc.topicTitle, tc.topicContent, tc.date FROM forumTopics tc JOIN user u ON tc.userId = u.id`);
-});
-app.get('/news/forumTopics/:topicId', (req, res) => {
-    const { topicId } = req.params;
-    const query = 'SELECT fp.topicId,u.usernames, fp.topicTitle, fp.topicContent, fp.date FROM forumTopics fp JOIN user u ON fp.userId = u.id WHERE topicId = ?';
-    queryDB(res, query, [topicId], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json(result);
-    });
-});
-app.get('/news/forumComments/:topicId', (req, res) => {
-    const { topicId } = req.params;
 
-    const query = 'SELECT tc.topicId, tc.commentId, tc.userId, u.usernames, tc.commentContent, tc.date FROM topicComments tc INNER JOIN user u ON tc.userId = u.id WHERE tc.topicId = ?';
-
-    queryDB(res, query, [topicId], (err, result) => {
-        if (err) {
-            console.error('Database error:', err.message);
-            return res.status(500).json({ error: 'Error fetching comments', details: err.message });
-        }
-        res.json(result);
-    });
-});
-app.get('/news/last-topicComment', (req, res) => {
-    const { topicId } = req.query; // Extract topicId from query parameters
-    const query = 'SELECT u.usernames, tc.date FROM topicComments tc JOIN user u ON tc.userId = u.id WHERE tc.topicId = ? ORDER BY ABS(TIMESTAMPDIFF(SECOND, tc.date, NOW())) LIMIT 1';
-
-    queryDB(res, query, [topicId], (err, result) => {
-        if (err) {
-            console.error('Database error:', err.message);
-            return res.status(500).json({ error: 'Error fetching comment', details: err.message });
-        }
-        res.json(result);
-    });
-});
-
-app.get('/news/popular-topics', (req, res) => {
-    queryDB(res, 'SELECT tc.topicId, ft.topicTitle, u.usernames, COUNT(tc.commentId) AS commentCount FROM topicComments tc JOIN user u ON tc.userId = u.id LEFT JOIN forumtopics ft ON tc.topicId = ft.topicId GROUP BY tc.topicId ORDER BY commentCount DESC LIMIT 10;');
-});
-app.delete('/news/forumTopics/:topicId', (req, res) => {
-    const { topicId } = req.params.id;
-    const query = 'DELETE FROM forumTopics WHERE topicId = ?';
-    queryDB(res, query, [topicId], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json({ message: 'Topic deleted successfully' });
-    });
-});
-// POST a new forum topic
-app.post('/news/forumTopics', (req, res) => {
-    const { userId, topicTitle, topicContent, date } = req.body;
-
-    const query = `
-        INSERT INTO forumTopics (userId, topicTitle, topicContent, date)
-        VALUES (?, ?, ?, ?)
-    `;
-    const values = [
-        userId,
-        topicTitle,
-        topicContent,
-        date
-    ];
-    queryDB(res, query, values);
-});
-app.post('/news/upload-comment', (req, res) => {
-    const { topicId, userId, commentContent, date } = req.body;
-    const query = 'INSERT INTO topicComments (topicId, userId, commentContent, date) VALUES (?, ?, ?, ?)';
-    const values = [topicId, userId, commentContent, date];
-
-    queryDB(res, query, values, (err, result) => {
-        if (err) {
-            console.error('Error inserting comment:', err);
-            return res.status(500).json({ error: 'Error inserting comment', details: err });
-        }
-        res.status(201).json({ id: result.insertId, topicId, userId, commentContent, date });
-    });
-});
 app.post('/news/register', (req, res) => {
     const { username, email, password } = req.body;
 
@@ -318,7 +161,7 @@ app.post('/news/register', (req, res) => {
             return res.status(500).json({ message: 'Error hashing password' });
         }
 
-        const query = 'INSERT INTO user (permission,usernames, emails, passwords) VALUES (3,?, ?, ?)';
+        const query = 'INSERT INTO user (usernames, emails, passwords) VALUES (?, ?, ?)';
         conn.query(query, [username, email, hashedPassword], (err, result) => {
             if (err) {
                 return res.status(500).json({ message: 'Error registering user', error: err.message });
@@ -359,19 +202,11 @@ app.post('/news/login', (req, res) => {
 });
 
 app.get('/news/get-profile', authorize, (req, res) => {
-    const query = 'SELECT id, usernames, emails, permission FROM user WHERE id = ?';
-    queryDB(res, query, [req.user.id]);
-});
-app.get('/news/get-all-profiles', authorize, (req, res) => {
-    const query = 'SELECT id ,usernames, permission FROM user';
+    const query = 'SELECT usernames, emails, permission FROM user WHERE id = ?';
     queryDB(res, query, [req.user.id]);
 });
 
-app.put('/news/update-permission', authorize, (req, res) => {
-    const { permission, id } = req.body;
-    const query = 'UPDATE user SET permission = ? WHERE id = ?';
-    queryDB(res, query, [permission, id]);
-});
+
 
 app.put('/news/update-username', authorize, (req, res) => {
     const { username } = req.body;
