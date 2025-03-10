092efe9a636b546d47f084b3e80548554efcc6d6
InancsiKrisztian1 <inancsikrisztian@outlook.com>
2025-03-10 22:22:47 +0100

    changes - 20250310
    Fórum oldal: Mostantól lehet reportolni a topikokat. Az Admin profil látja az összes reportot, legfrissebbtől a legrégebbiig.
    loader animáció minden oldalra, addig amíg be nem tölt a tartalom (loader.html, loader-script.js).

--- temp/news-1/application/server/public/js/frontend/header-renderer.js
+++ temp/news-1/application/server/public/js/frontend/header-renderer.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const headerContainer = document.querySelector('header');
+    let logoText = 'F1 News';
+
+    if (window.location.pathname.includes('/news/about.html') || window.location.pathname.includes('/news/auth.html')) {
+        logoText = 'F1 Stats &amp; News';
+    } else if (window.location.pathname.includes('/news/forum-layout.html')) {
+        logoText = 'F1 Forum';
+    }
 
     headerContainer.innerHTML += `
-        
-            <div class="logo">
-                <h2><a href="/news/index.html">F1 News</a></h2>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="#" id="latest-btn">Latest</a></li>
-                    <li><a href="#" id="schedule-btn">Schedule</a></li>
-                    <li><a href="#" id="result-btn">Results</a></li>
-                    <li><a href="#standlist">Standlist</a></li>
-                </ul>
-            </nav>
+        <div class="logo">
+            <h2><a href="/news/index.html">${logoText}</a></h2>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#" id="latest-btn">Latest</a></li>
+                <li><a href="#" id="schedule-btn">Schedule</a></li>
+                <li><a href="#" id="result-btn">Results</a></li>
+                <li><a href="#standlist">Standlist</a></li>
+            </ul>
+        </nav>
     `;
 });
---
+++ temp/news-1/application/server/public/js/frontend/loader-script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    document.getElementById('loader-frame').style.display = 'none';
+    document.querySelector('.content').style.display = 'block';
+});
